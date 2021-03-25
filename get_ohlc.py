eb322eb309e95412b1e51448d570b63c568fff27
Mike Vollbracht <Mike.Vollbracht@gmail.com>
2021-03-25 11:55:41 -0500

    begin update logic

--- get_ohlc.py
+++ get_ohlc.py
@@ -5,6 +5,7 @@ import yfinance as yf
 import os 
 import datetime
 import time
+import pandas as pd
 
 #override for data reader
 yf.pdr_override()
@@ -28,23 +29,29 @@ def get_ohlc():
 
     for count, ticker in enumerate(tickers): 
         if not os.path.exists(f'ohlc_data/{ticker}.csv'):
-            ohlc_data = pdr.get_data_yahoo(ticker, start=start_date ,end=end_date)
-            call_count += 1
+            # ohlc_data = pdr.get_data_yahoo(ticker, start=start_date ,end=end_date)
+            # call_count += 1
         
-            if ohlc_data.shape[0] > 0: 
-                ohlc_data.to_csv(f'ohlc_data/{ticker}.csv')
-            else: 
-                with open('ohlc_data/error_log.txt', 'a') as error_log:
-                    error_log.write(f'{run_timestamp}: {ticker} not found\n')
+            # if ohlc_data.shape[0] > 0: 
+            #     ohlc_data.to_csv(f'ohlc_data/{ticker}.csv')
+            # else: 
+            #     with open('ohlc_data/error_log.txt', 'a') as error_log:
+            #         error_log.write(f'{run_timestamp}: {ticker} not found\n')
                     
-            #To prevent throttling by yahoo
-            if call_count % batch_max == 0: 
-                call_count = 0
-                print(f'Ticker Count: {count}, waiting to prevent throttling.')
-                time.sleep(2)
+            # #To prevent throttling by yahoo
+            # if call_count % batch_max == 0: 
+            #     call_count = 0
+            #     print(f'Ticker Count: {count}, waiting to prevent throttling.')
+            #     time.sleep(2)
+            pass
                     
         else: 
             print(f'Already have {ticker}')
-            # Get last date in the data frame and use as the start 
+            #open existing csv 
+            df = pd.read_csv(f'ohlc_data/{ticker}.csv', index_col=0)
+            # Get last date in the df and add 1 date 
+            last_date = datetime.datetime.strptime(df.index.values[-1],'%Y-%m-%d').date() + datetime.timedelta(days=1)
+            print(last_date)
             
-    
+
+# get_ohlc()
