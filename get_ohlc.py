92a77709193c9b8743809f37febf22da8eaeeb21
Mike Vollbracht <Mike.Vollbracht@gmail.com>
2021-03-25 12:02:16 -0500

    updated git ignore

--- get_ohlc.py
+++ get_ohlc.py
@@ -38,20 +38,23 @@ def get_ohlc():
             #     with open('ohlc_data/error_log.txt', 'a') as error_log:
             #         error_log.write(f'{run_timestamp}: {ticker} not found\n')
                     
-            # #To prevent throttling by yahoo
-            # if call_count % batch_max == 0: 
-            #     call_count = 0
-            #     print(f'Ticker Count: {count}, waiting to prevent throttling.')
-            #     time.sleep(2)
+           
             pass
                     
         else: 
-            print(f'Already have {ticker}')
             #open existing csv 
             df = pd.read_csv(f'ohlc_data/{ticker}.csv', index_col=0)
             # Get last date in the df and add 1 date 
             last_date = datetime.datetime.strptime(df.index.values[-1],'%Y-%m-%d').date() + datetime.timedelta(days=1)
-            print(last_date)
             
+            #cant have same date
+            if last_date < end_date:
+                print(f'Updating {ticker}')
+
+        #To prevent throttling by yahoo
+        if call_count % batch_max == 0: 
+            call_count = 0
+            print(f'Ticker Count: {count}, waiting to prevent throttling.')
+            time.sleep(2)
 
 # get_ohlc()
