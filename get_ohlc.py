a595cbdb174ce065ba9715554e15424fe0d86d8d
Mike Vollbracht <mike.vollbracht@gmail.com>
2021-03-27 12:42:27 -0500

    clean data before saving

--- get_ohlc.py
+++ get_ohlc.py
@@ -1,49 +1,62 @@
-#Resource https://pypi.org/project/yfinance/
+# Resource https://pypi.org/project/yfinance/
 
 from pandas_datareader import data as pdr
+import pandas as pd
 import yfinance as yf
-import os 
+import os
 import datetime
 import time
 
-#override for data reader
+# override for data reader
 yf.pdr_override()
 
-#time of run for error log 
+# time of run for error log
 run_timestamp = datetime.datetime.now()
 start_date = '2016-01-01'
 end_date = datetime.date.today()
 
-#pause after 10 to prevent throttling 
+# pause after 10 to prevent throttling
 batch_max = 10
 call_count = 0
 
-#get tickers
+# get tickers
 tickers = pd.read_csv('tickers/my_tickers.csv')
 tickers = tickers['Ticker']
+
 
 def get_ohlc():
     if not os.path.isdir('ohlc_data'):
         os.makedirs('ohlc_data')
-    
+
     if not os.path.isdir('logs'):
         os.makedirs('logs')
 
-    for count, ticker in enumerate(tickers): 
+    for count, ticker in enumerate(tickers):
         if not os.path.exists(f'ohlc_data/{ticker}.csv'):
-            ohlc_data = pdr.get_data_yahoo(ticker, start=start_date ,end=end_date)
+            ohlc_data = pdr.get_data_yahoo(
+                ticker, start=start_date, end=end_date)
             call_count += 1
-        
-            if ohlc_data.shape[0] > 0: 
+
+            if ohlc_data.shape[0] > 0:
+                # clean missing rows
+                ohlc_data.fillna(method='ffill', inplace=True)
+                ohlc_data.fillna(method='bfill', inplace=True)
+                # clean volume
+                ohlc_data['Volume'].replace(
+                    to_replace=0, method='ffill', inplace=True)
+                ohlc_data['Volume'].replace(
+                    to_replace=0, method='bfill', inplace=True)
                 ohlc_data.to_csv(f'ohlc_data/{ticker}.csv')
-            else: 
+            else:
                 with open('logs/ohlc_error_log.txt', 'a') as error_log:
                     error_log.write(f'{run_timestamp}: {ticker} not found\n')
-                
-        #To prevent throttling by yahoo
-        if call_count % batch_max == 0: 
+
+        # To prevent throttling by yahoo
+        if call_count % batch_max == 0:
             call_count = 0
-            print(f'Tickers Processed: {count}, waiting to prevent throttling.')
+            print(
+                f'Tickers Processed: {count}, waiting to prevent throttling.')
             time.sleep(2)
+
 
 get_ohlc()
